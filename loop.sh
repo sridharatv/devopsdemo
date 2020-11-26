42505bb5fe646a4c4cb8756012d2189d451f9960
Sridhara T V <sridharatv@gmail.com>
2020-11-26 07:31:18 +0000

    Revert "created a function"
    This reverts commit 175bab8ab94b9ac4a3586833b674f22dc19a99d2.

--- loop.sh
+++ loop.sh
@@ -4,23 +4,15 @@ echo "this is a loop shell script"
 
 # we need to add a graceful exit
 c=0
-
-gen_rand()
-{
-	while $true
-	do
-		c=`expr $c + 1`
-		echo $RANDOM
-		sleep 1
-		if [ $c -eq $1 ]; then
-			break
-		fi
-	done
-}
-
-gen_rand 10
-echo -e "\n\n Generating 20 times\n\n"
-gen_rand 20
+while $true
+do
+	c=`expr $c + 1`
+	echo $RANDOM
+	sleep 2
+	if [ $c -eq 20 ]; then
+		break
+	fi
+done
 
 echo "done"
 
