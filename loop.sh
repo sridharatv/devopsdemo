7e1cc5f46a0c93d85cfecce3a7d28d2d97fcc2df
Sridhara T V <sridharatv@gmail.com>
2020-11-26 07:38:14 +0000

    fixed

--- loop.sh
+++ loop.sh
@@ -3,10 +3,15 @@
 echo "this is a loop shell script"
 
 # we need to add a graceful exit
+c=0
 while $true
 do
+	c=`expr $c + 1`
 	echo $RANDOM
 	sleep 1
+	if [ $c -eq 10 ]; then
+		break
+	fi
 done
 
 echo "done"
